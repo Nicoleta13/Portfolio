197baa63613a5e467bb6923110444d992660f5ad
Nicoleta13 <39745045+Nicoleta13@users.noreply.github.com>
2019-01-16 14:02:21 +0200

    add changes

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
-// Text transition
-function scrollAppear(){
-  var text = document.querySelector(".text");
-  var introPosition = text.getBoundingClientRect().top;
-  var screenPosition = window.innerHeight / 2;
+  // Text transition
+  function scrollAppear(){
+    var text = document.querySelector(".text");
+    var introPosition = text.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 2;
 
-  if(introPosition < screenPosition) {
-    text.classList ='appear';
+    if(introPosition < screenPosition) {
+      text.classList ='appear';
+    }
   }
-}
 
-window.addEventListener('scroll', scrollAppear);
-
+  window.addEventListener('scroll', scrollAppear);
 
 
-// circular image
-(function scaleImage(){
-  const element = document.querySelector('.circular-reveal');
-  const inner = document.querySelector('.circular-reveal-content');
 
-  const easing = 0.1;
-  const outScale = 0.6;
-  const inScale = 1;
-  let targetScale = outScale;
-  let elementScale = targetScale;
-  let innerScale = 1 / elementScale;
-  element.addEventListener("pointerover", () => {
-    targetScale = inScale;
+  // circular image
+  (function scaleImage(){
+    const element = document.querySelector('.circular-reveal');
+    const inner = document.querySelector('.circular-reveal-content');
+
+    const easing = 0.1;
+    const outScale = 0.6;
+    const inScale = 1;
+    let targetScale = outScale;
+    let elementScale = targetScale;
+    let innerScale = 1 / elementScale;
+    element.addEventListener("pointerover", () => {
+      targetScale = inScale;
+      });
+
+    element.addEventListener("pointerout", () => {
+      targetScale = outScale;
     });
 
-  element.addEventListener("pointerout", () => {
-    targetScale = outScale;
-  });
+    const update = ()  => {
+      elementScale +=  (targetScale - elementScale) * easing;
+      innerScale =  1/elementScale;
+      element.style.transform = `scale(${elementScale})`;
+      inner.style.transform = `scale(${innerScale})`;
+    requestAnimationFrame(update);
+  };
 
-  const update = ()  => {
-    elementScale +=  (targetScale - elementScale) * easing;
-    innerScale =  1/elementScale;
-    element.style.transform = `scale(${elementScale})`;
-    inner.style.transform = `scale(${innerScale})`;
   requestAnimationFrame(update);
-};
-
-requestAnimationFrame(update);
-})();
+  })();
